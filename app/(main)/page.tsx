0cfa1099dfdaf2f854df6826e78e5d64557ea1d9
nicohermida01 <nshermida101@gmail.com>
2023-07-08 13:59:51 -0300

    add No tweets found text

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,25 +6,25 @@ import { getRelativeTime } from 'utils/getRelativeTime'
 export default async function IndexPage() {
 	const tweets = await tweetService.getAllTweets()
 
-	if (!tweets) {
-		return <span>No tweets found</span>
-	}
-
 	return (
-		<div className='flex flex-col gap-4'>
-			{tweets.map(tweet => (
-				<Link
-					href={`/tweet/${tweet.id}`}
-					key={tweet.id}
-					className='bg-topbar-background-black p-4 rounded-md text-[14px] flex flex-col gap-2'
-				>
-					<div className='flex justify-between text-[12px] text-text-secondary'>
-						<span className=''>{`@${tweet.user.username}`}</span>
-						<span>{getRelativeTime(tweet.createdAt)}</span>
-					</div>
-					{tweet.content}
-				</Link>
-			))}
+		<div className='flex flex-col gap-4 items-center'>
+			{tweets.length > 0 ? (
+				tweets.map(tweet => (
+					<Link
+						href={`/tweet/${tweet.id}`}
+						key={tweet.id}
+						className='bg-topbar-background-black p-4 rounded-md text-[14px] flex flex-col gap-2'
+					>
+						<div className='flex justify-between text-[12px] text-text-secondary'>
+							<span className=''>{`@${tweet.user.username}`}</span>
+							<span>{getRelativeTime(tweet.createdAt)}</span>
+						</div>
+						{tweet.content}
+					</Link>
+				))
+			) : (
+				<span className='text-text-secondary'>~ No tweets found ~</span>
+			)}
 		</div>
 	)
 }
